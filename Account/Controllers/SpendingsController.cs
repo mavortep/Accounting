93ceb83bee2916e11bba14f944f5a195d103b3f3
mavortep <redhotchilipeppers.mp@gmail.com>
2018-02-27 19:26:21 +0200

    Initial commit

---
+++ Account/Controllers/SpendingsController.cs
@@ -0,0 +1,100 @@
+﻿using Account.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Account.Controllers
+{
+    public class SpendingsController : Controller
+    {
+        // GET: Spendings
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllSpendings());
+        }
+
+        IEnumerable<Spendings> GetAllSpendings()
+        {
+            using (CashJugDBEntities db = new CashJugDBEntities())
+            {
+                return db.Spendings.ToList<Spendings>();
+            }
+        }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            Spendings spend = new Spendings();
+            if (id != 0)
+            {
+                using (CashJugDBEntities db = new CashJugDBEntities())
+                {
+                    spend = db.Spendings.Where(x => x.ItemID == id).FirstOrDefault<Spendings>();
+                }
+            }
+            return View(spend);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Spendings spend)
+        {
+            try
+            {
+                if (spend.ImageUpload != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(spend.ImageUpload.FileName);
+                    string extension = Path.GetExtension(spend.ImageUpload.FileName);
+                    fileName = fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                    spend.ImagePath = "~/AppFiles/Images/" + fileName;
+                    spend.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/Images/"), fileName));
+                }
+                using (CashJugDBEntities db = new CashJugDBEntities())
+                {
+                    if (spend.ItemID == 0)
+                    {
+                        db.Spendings.Add(spend);
+                        db.SaveChanges(); 
+                    }
+                    else
+                    {
+                        db.Entry(spend).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                }
+                return RedirectToAction("Index");
+                //return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "Index", GetAllSpendings()), message = "Submitted successfully" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = true, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (CashJugDBEntities db = new CashJugDBEntities())
+                {
+                    Spendings spend = db.Spendings.Where(x => x.ItemID == id).FirstOrDefault<Spendings>();
+                    db.Spendings.Remove(spend);
+                    db.SaveChanges();
+                }
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllSpendings()), message = "Deleted successfully" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = true, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ Account/Scripts/CRUDScript.js
@@ -0,0 +1,97 @@
+﻿$(function () {
+    $("#loaderbody").addClass('hide');
+
+
+    $(document).bind('ajaxStart', function () {
+        $('#loaderbody').removeClass('hide');
+    }).bind('ajaxStop', function () {
+        $("#loaderbody").addClass('hide');
+    })
+});
+
+function ShowImagePreview(imageUploader, previewImage) {
+    if (imageUploader.files && imageUploader.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(previewImage).attr('src', e.target.result);
+        }
+        reader.readAsDataURL(imageUploader.files[0]);
+    }
+}
+
+function AccountPost(form) {
+    $.validator.unoburstive.parse(form);
+    if ($(form).valid()) {
+        var ajaxConfig = {
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),
+            success: function (response) {
+                if (response.success) {
+                    $("#1").html(response.html);
+                    refreshAddNewTab($(form).attr('data-restUrl'), true);
+                    $.notify(response.message, "success");
+                    if (typeof activatejQueryTable !== 'undefined' && $.isFunction(activatejQueryTable)) {
+                        activatejQueryTable();
+                    }
+                }
+                else {
+                    $.notify(response.message, "error");
+                }
+            }
+        }
+        if ($(form).attr('enctype') == "multipart/form-data") {
+            ajaxConfig["contentType"] = false;
+            ajaxConfig["processData"] = false;
+        }
+        $.ajax(ajaxConfig);
+    }
+    return false;
+}
+
+function refreshAddNewTab(resetUrl, showViewTab) {
+    $.ajax({
+        type: 'GET',
+        url: resetUrl,
+        success: function (response) {
+            $("#2").html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html('Add New');
+            if (showViewTab) 
+                $('ul.nav.nav-tabs a:eq(0)').tab('show');
+        }
+    })
+}
+
+function Edit(url) {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function (response) {
+            $("#2").html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html('Edit');
+            $('ul.nav.nav-tabs a:eq(1)').tab('show');
+        }
+    })
+}
+
+function Delete(url) {
+    if (confirm('Are you sure you want to delete this item?') == true) {
+    $.ajax({
+            type: 'POST',
+            url: url,
+            success: function (response) {
+                if (response.success) {
+                    $("#1").html(response.html);
+                    $.notify(response.message, "warn");
+                    if (typeof activatejQueryTable !== 'undefined' && $.isFunction(activatejQueryTable)) {
+                        activatejQueryTable();
+                    }
+                }
+                else {
+                    $.notify(response.message, "error"); 
+                }
+            }
+        })
+    }
+}
+//
